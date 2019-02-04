73bf34f9c7b171e01aca2165cdbb8ef0fb78ec0b
asnr <asnr@users.noreply.github.com>
2019-02-03 19:29:15 -0800

    Control snake slither direction with arrow keys

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import 'normalize.css';
 import { BOARD_TILE_WIDTH, BOARD_TILE_HEIGHT } from './constants';
-import { DIRECTIONS, initialModel, updateModel } from './model';
+import { keyPressed, initialModel, updateModel } from './model';
 
 const MILLISECONDS_BETWEEN_UPDATES = 300;
 
@@ -10,7 +10,10 @@ const TILE_HEIGHT = 10;
 const BOARD_PIXEL_WIDTH = TILE_WIDTH * BOARD_TILE_WIDTH;
 const BOARD_PIXEL_HEIGHT = TILE_HEIGHT * BOARD_TILE_HEIGHT;
 
+
 const startGame = () => {
+  window.addEventListener('keydown', keyPressed, false);
+
   const canvas = document.getElementById('snake-canvas');
   canvas.width = BOARD_PIXEL_WIDTH;
   canvas.height = BOARD_PIXEL_HEIGHT;
--- src/model.js
+++ src/model.js
@@ -1,26 +1,62 @@
 import { BOARD_TILE_WIDTH, BOARD_TILE_HEIGHT } from './constants';
 
+// String constants are defined by key value HTML spec
+//   https://developer.mozilla.org/en-US/docs/Web/API/KeyboardEvent/key/Key_Values
+const DIRECTION_KEY = {
+  LEFT: 'ArrowLeft',
+  DOWN: 'ArrowDown',
+  RIGHT: 'ArrowRight',
+  UP: 'ArrowUp',
+};
 
-export const DIRECTIONS = {
-  UP: { x: 0, y: -1 },
+const DIRECTION = {
+  [DIRECTION_KEY.LEFT]: { x: -1, y: 0 },
+  [DIRECTION_KEY.DOWN]: { x: 0, y: 1 },
+  [DIRECTION_KEY.RIGHT]: { x: 1, y: 0 },
+  [DIRECTION_KEY.UP]: { x: 0, y: -1 },
+};
+
+let nextDirection = null;
+let currentDirection = DIRECTION_KEY.UP;
+
+const ALLOWED_DIRECTIONS = {
+  [DIRECTION_KEY.LEFT]: [DIRECTION_KEY.UP, DIRECTION_KEY.DOWN],
+  [DIRECTION_KEY.DOWN]: [DIRECTION_KEY.LEFT, DIRECTION_KEY.RIGHT],
+  [DIRECTION_KEY.RIGHT]: [DIRECTION_KEY.UP, DIRECTION_KEY.DOWN],
+  [DIRECTION_KEY.UP]: [DIRECTION_KEY.LEFT, DIRECTION_KEY.RIGHT],
 };
 
 
+export const keyPressed = ({ key }) => {
+  if (!nextDirection && directionIsAllowed(key)) {
+    nextDirection = key;
+  }
+  keyPressedEvent.preventDefault();
+};
+
+
+export const directionIsAllowed = key =>
+  ALLOWED_DIRECTIONS[currentDirection].find(x => x === key) !== undefined;
+
+
 export const initialModel = {
-  direction: DIRECTIONS.UP,
   snake: [{ x: 5, y: 17 }, { x: 6, y: 17 }, { x: 7, y: 17 }],
 };
 
 
-export const updateModel = (model) => {
-  const { direction, snake } = model;
+export const updateModel = ({ snake }) => {
+  const directionKey = nextDirection ? nextDirection : currentDirection;
+  nextDirection = null;
+
+  const direction = DIRECTION[directionKey];
   const head = snake[0];
   const nextHead = {
     x: (BOARD_TILE_WIDTH + head.x + direction.x) % BOARD_TILE_WIDTH,
     y: (BOARD_TILE_HEIGHT + head.y + direction.y) % BOARD_TILE_HEIGHT,
   };
+
+  currentDirection = directionKey;
   return {
-    direction,
     snake: [nextHead, ...snake.slice(0, -1)],
   };
 };
